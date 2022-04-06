e38f33af97116053be6329061e6aa07e6d4eb6c5
Bruno Krauthamer <brunokrauthamer@gmail.com>
2022-04-06 18:46:27 -0300

    Exercicio 5

---
+++ fundamentos/boloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio5.js
@@ -0,0 +1,13 @@
+const a = 0
+const b = 0
+const c = 180
+
+if (a+b+c !== 180) {
+    console.log("false")
+}
+else if (a<=0 || b <=0 || c<= 0) {
+    console.log("false")
+}
+else {
+    console.log("true")
+}
