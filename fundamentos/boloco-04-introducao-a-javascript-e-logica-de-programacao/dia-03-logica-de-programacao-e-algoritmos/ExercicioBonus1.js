9b5d3af45408bc0eb35f364f7ea06d79a2a2f3cc
Bruno Krauthamer <brunokrauthamer@gmail.com>
2022-04-08 19:49:38 -0300

    exercicio bonus 1

---
+++ fundamentos/boloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/ExercicioBonus1.js
@@ -0,0 +1,19 @@
+const n = 10;
+// Creating each line string
+
+let string = ''
+for (let index = 0; index < n; index += 1) {
+    string += '*'
+}
+
+for (let index = 0; index < n; index += 1) {
+    console.log(string);
+}
+
+
+
+
+
+
+
+
